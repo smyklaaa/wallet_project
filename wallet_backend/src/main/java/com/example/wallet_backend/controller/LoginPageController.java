0564458520b53bca711eb7fd3390215d1881ead7
purple_orange_beach <52859277+smyklaaa@users.noreply.github.com>
2024-05-17 14:05:02 +0200

    first commit

---
+++ wallet_backend/src/main/java/com/example/wallet_backend/controller/LoginPageController.java
@@ -0,0 +1,7 @@
+package com.example.wallet_backend.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class LoginPageController {
+}
---
+++ wallet_backend/src/main/java/com/example/wallet_backend/service/UserService.java
@@ -0,0 +1,7 @@
+package com.example.wallet_backend.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+}
