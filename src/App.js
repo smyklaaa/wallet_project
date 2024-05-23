98327d9cded3c560a39d5677eedf1271d09a3860
purple_orange_beach <52859277+smyklaaa@users.noreply.github.com>
2024-05-23 15:28:29 +0200

    basic login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
+import {
+    BrowserRouter as Router,
+    Route,
+    Routes
+} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from "./Home";
+import LoginPage from "./LoginPage";
+
+export default function App() {
+    return (
+        <Router>
+            <div className="App">
+                {/*<Header />*/}
+                <nav>
+                    <ul>
+                    </ul>
+                </nav>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/login-page" element={<LoginPage/>}/>
+                </Routes>
+
+                {/*<Footer />*/}
+            </div>
+        </Router>
+    );
 }
-
-export default App;
---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+        return (
+            <nav>
+                <ul>
+                    <li><a href="home">Home</a></li>
+                    <li><a href="login-page">LoginPage</a></li>
+                </ul>
+            </nav>
+        );
+};
+
+export default Home;
