1fb0d43b5c0c9dd7c110bd235320d5df47ca85a0
Marysek17 <sebastian.mlodzik@student.pk.edu.pl>
2024-05-31 22:52:37 +0200

    prepare for PR

--- wallet_backend/src/main/java/com/example/wallet_backend/controller/RegistrationPageController.java
+++ wallet_backend/src/main/java/com/example/wallet_backend/controller/RegistrationPageController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/registration-page")
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:3000")
 public class RegistrationPageController {
 
     @Autowired
--- wallet_backend/src/main/java/com/example/wallet_backend/controller/SecurePageController.java
+++ wallet_backend/src/main/java/com/example/wallet_backend/controller/SecurePageController.java
@@ -2,6 +2,7 @@ package com.example.wallet_backend.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import javax.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping("/secure")
+@CrossOrigin(origins = "http://localhost:3000")
 public class SecurePageController {
 
     @GetMapping("/welcome")
